4e2ceb57892a8e21bc20271bb43c5d5474eda9f9
Michal Ziemianek <mziemianek3@gmail.com>
2025-02-27 17:36:05 +0100

    basic hello world api

--- services/cards/src/main.go
+++ services/cards/src/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to start the cards service", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
